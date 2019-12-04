e7da0192928bfc798d2651eb77a7d47fc055a3cc
WilliamBergamin <Berw@videotron.ca>
2019-12-04 18:39:33 -0500

    Major update

---
+++ readme.md
@@ -0,0 +1,17 @@
+# Dynamic Resume
+
+## What
+
+This Dynamic Resume is a presonal website that is generated dynamicly from a JSON object that contains resume information. It can easily be deployed to github pages.
+
+## Why
+
+Software developers are known to be lazy. This why I have made this tool. With very little effort I can update the website.
+
+## Who should use it
+
+This project was made for me, I am not expecting anyone to use it. But if you want go ahead. All you need to do is update the resume.js file and change le links to your github and linkedIn.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ resume.js
@@ -0,0 +1,132 @@
+const resume = {
+    "name": "William R. Bergamin",
+    "linkedin": "https://www.linkedin.com/in/william-r-bergamin-34536b135",
+    "email": "william.bergamin.coen@gmail.com",
+    "phone": "514 503 3126",
+
+    // SUMMARY OF SKILLS AND QUALIFICATIONS
+    "languages": ["French│Spoken & Written", "English│Spoken & Written"],
+    "programming": ["Python", "Java", "JavaScript", "C/C++", "Tensorflow", "Keras", "VHDL"],
+    "other": ["Kubernetes", "Docker", "Git"],
+    //EDUCATION
+    "education": [
+        {
+            "degree": "Bachelor of Engineering – Computer Engineering Co-op",
+            "date": "2020 (Expected)",
+            "school": "Concordia University, Montreal, QC",
+            "points": ["Member of the Institute for Co-operative Education", "Relevant courses: Data Structures & Algorithm, Operating Systems, Cloud Computing"]
+        },
+        {
+            "degree": "DEC in Pure & Applied Science",
+            "date": "2016",
+            "school": "Dawson College, Montréal, QC",
+            "points": []
+        }
+    ],
+
+    // WORK
+    "work": [
+        {
+            "position": "Machine Learning Intern",
+            "date": "2019-Now",
+            "location": "Beast Group, Westmount, QC",
+            "points": [
+                "Responsible for the development of deep learning networks",
+                "Built neural networks in Tensorflow and Keras",
+                "Learned the inner workings of neural networks and how to properly train one"
+            ]
+        },
+        {
+            "position": "Software Developer",
+            "date": "2017-2018",
+            "location": "TD Insurance, Villery-Saint-Michel-Parc Extention, Qc",
+            "points": [
+                "Responsible for the development of JAVA APIs",
+                "Updated out-dated back-end services",
+                "Learned and understood the front to back-end components of TD Insurance services",
+            ]
+        },
+    ],
+
+    // PROJECTS
+    "project": [
+        {
+            "name": "McGill RoboHacks",
+            "date": "2018",
+            "points": [
+                "The Project won 1st place overall",
+                "Built shirt-folding robot with AI personality",
+                "Used Arduino and AI technologies"
+            ]
+        },
+        {
+            "name": "Concordia BlockHacks",
+            "date": "2018",
+            "points": [
+                "The Project won 2nd place overall",
+                "Created 2 Android applications that could communicate with each other",
+                "Used JAVA, RAML, Android Studio & Firebase to create the applications"
+            ]
+        },
+        {
+            "name": "McGill RoboHacks",
+            "date": "2017",
+            "points": [
+                "The Project won \"Best Electrical Robot\"",
+                "Built ShotBot robot, capable of detecting and filling up to 6 glasses with liquid, in 24hr time frame",
+                "Used Arduino and Java to control the robot from a smartphone"
+            ]
+        },
+        {
+            "name": "ConUhack ll",
+            "date": "2017",
+            "points": [
+                "Used Arduino and C++ to create, a small videogame console power by a small microcontroller controlled through a prototyped gamepad and broadcasted onto a matrix of LED",
+                "Project “BreadBox” was created and presented in front of judges"
+            ]
+        },
+    ],
+
+    // PROFESSIONAL ASSOCIATIONS
+    "association": [
+        {
+            "position": "Vice President of External Affaires for the Engineering & Computer Science Association",
+            "date": "2019 – Now",
+            "location": "Concordia University Hall 838, Montréal, QC",
+            "points": [
+                "Represent Concordia Engineering & Computer Science students on a National and Provincial level",
+                "Serving and representing almost 6000 students",
+                "Responsible of all external networking for the association"
+            ]
+        },
+        {
+            "position": "Vice President of Social Affaires for the Engineering & Computer Science Association",
+            "date": "2018 – 2019",
+            "location": "Concordia University Hall 838, Montréal, QC",
+            "points": [
+                "Organize and coordinate all social events for students in the faculty of engineering and computer science at Concordia University",
+                "Serving and representing almost 6000 students",
+                "Managing the budget for all social events organised by the association",
+                "Negotiating all contracts pertaining to social events and activities"
+            ]
+        },
+        {
+            "position": "Director of Social Affaires for the Engineering & Computer Science Association",
+            "date": "2017 – 2018",
+            "location": "Concordia University Hall 838, Montréal, QC",
+            "points": [
+                "Assist the Vice President of Social Affairs",
+                "Organize and coordinate social events in the goal of getting students involved at Concordia",
+            ]
+        },
+        {
+            "position": "Engineering Games Concordia Delegation",
+            "date": "2017 – 2019",
+            "location": "Concordia University Hall 838, Montréal, QC",
+            "points": [
+                "Association that coordinates building of drones that will then compete against other Universities",
+                "Participated in the building of software (C++) to control and operate the machine",
+            ]
+        },
+    ]
+}
